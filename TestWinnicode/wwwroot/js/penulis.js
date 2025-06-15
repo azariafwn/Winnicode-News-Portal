1bdc5c83908e4fdf2ea13a1ea8a7a1636e9d9401
zafawniar <zafawniar@gmail.com>
2025-06-15 14:56:48 +0700

    update dashboard penulis

---
+++ TestWinnicode/wwwroot/js/penulis.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.getElementById('sidebarMenu');
+    const toggleBtn = document.querySelector('[data-bs-target="#sidebarMenu"]');
+    const overlay = document.getElementById('sidebarOverlay');
+
+    if (toggleBtn && sidebar && overlay) {
+        toggleBtn.addEventListener('click', () => {
+            sidebar.classList.toggle("show");
+            overlay.classList.toggle("show");
+        });
+
+        overlay.addEventListener('click', () => {
+            sidebar.classList.remove("show");
+            overlay.classList.remove("show");
+        });
+    }
+});
